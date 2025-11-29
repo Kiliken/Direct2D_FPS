ee8b4743640966c6dff02d622c8279d06369e575
VincentCYR7 <yuren1027@gmail.com>
2025-11-29 15:57:08 +0900

    Squashed commit of the following:
    commit 2cc755495af3f3092647acc76b30e793075ff3f0
    Author: Zayar Aung <z.zayar99@gmail.com>
    Date:   Sat Nov 29 15:21:22 2025 +0900
    
        room draw test
    
    commit 695e1fd12794c5816ee8702c5558a33ef5ee9d59
    Author: Kiliken <francescopaolomariani7@gmail.com>
    Date:   Sat Nov 22 18:36:11 2025 +0900
    
        Some Other Tests

--- src/Player.h
+++ src/Player.h
@@ -5,47 +5,14 @@
 
 class Player
 {
-private:
-    ID2D1SolidColorBrush *pBrush;
-    ID2D1SolidColorBrush *pBrush2;
-
 public:
-    Player(ID2D1HwndRenderTarget &renderer);
-    ~Player();
+    explicit Player() {}
+    ~Player() {}
 
-    void Update();
-    void Draw(ID2D1HwndRenderTarget &renderer);
+    void Update() {}
 
-    D2D_POINT_2F pos = {200, 200};
-    D2D1_ELLIPSE pointer = D2D1::Ellipse(
-        pos ,2, 2);
-    D2D1_ELLIPSE body = D2D1::Ellipse(
-        pos, 20.0f, 20.0f);
-    
-    D2D_POINT_2F facingDir = {0, -1};
+    D2D_POINT_2F pos = {12.0f, 12.0f};
+    float angle = 0.0f; // radians
+    float moveSpeed = 4.0f; // tiles per second basis; scaled by dt
+    float rotSpeed = 1.8f;  // radians per second
 };
-
-Player::Player(ID2D1HwndRenderTarget &renderer)
-{
-    renderer.CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::NavajoWhite), &pBrush);
-    renderer.CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Thistle), &pBrush2);
-}
-
-Player::~Player()
-{
-}
-
-void Player::Update()
-{
-    body.point = pos;
-    pointer.point.x = pos.x + facingDir.x * 20;
-    pointer.point.y = pos.y + facingDir.y * 20;
-
-}
-
-void Player::Draw(ID2D1HwndRenderTarget &renderer)
-{
-    
-    renderer.FillEllipse(body, pBrush2);
-    renderer.FillEllipse(pointer, pBrush);
-}
